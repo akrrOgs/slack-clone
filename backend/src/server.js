0c50e9a0a04ccacbd3572ce696ae3e9094c70545
akrrOgs <dlordrog@gmail.com>
2025-08-28 20:36:24 +0700

    init

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
